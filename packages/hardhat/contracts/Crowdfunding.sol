a392d3f6dc10bf23337fa68717c919acd05f40eb
jotaro <nqtien0507@gmail.com>
2024-06-11 10:43:24 +0700

    Init test case

--- packages/hardhat/contracts/Crowdfunding.sol
+++ packages/hardhat/contracts/Crowdfunding.sol
@@ -8,8 +8,7 @@ import "hardhat/console.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
+    * @title Crowdfunding
  * @author jotaro
  */
 
