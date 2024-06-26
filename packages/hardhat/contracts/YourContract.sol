6ec51953a93c68d649b3ff34a53e34574d4af6ee
unknown <pdquan20@gmail.com>
2024-06-26 18:01:17 +0700

    init challenge-4-dex

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,66 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint);
+}
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
 contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+    IERC20 public token;
+    uint public tokenReserve;
+    uint public ethReserve;
+    uint public k;
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+    // Function to add liquidity to the pool
+    function addLiquidity(uint _tokenAmount) public payable {
+        uint ethAmount = msg.value;
+        if (tokenReserve > 0 && ethReserve > 0) {
+            require(ethAmount * tokenReserve == _tokenAmount * ethReserve, "Invalid ratio");
+        }
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+        tokenReserve += _tokenAmount;
+        ethReserve += ethAmount;
+        k = tokenReserve * ethReserve;
+    }
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
+    // Function to remove liquidity from the pool
+    function removeLiquidity(uint _tokenAmount, uint _ethAmount) public {
+        require(_tokenAmount <= tokenReserve && _ethAmount <= ethReserve, "Not enough liquidity");
+        token.transfer(msg.sender, _tokenAmount);
+        payable(msg.sender).transfer(_ethAmount);
+        tokenReserve -= _tokenAmount;
+        ethReserve -= _ethAmount;
+    }
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
-	}
+    // Function to swap ETH for Tokens
+    function swapEthForTokens() public payable {
+        uint ethAmount = msg.value;
+        uint tokenAmount = getAmountOut(ethAmount, ethReserve, tokenReserve, k);
+        require(token.transfer(msg.sender, tokenAmount), "Failed to transfer tokens");
+        ethReserve += ethAmount;
+        tokenReserve -= tokenAmount;
+    }
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
+    // Function to swap Tokens for ETH
+    function swapTokensForEth(uint _tokenAmount) public {
+        uint ethAmount = getAmountOut(_tokenAmount, tokenReserve, ethReserve, k);
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+        payable(msg.sender).transfer(ethAmount);
+        tokenReserve += _tokenAmount;
+        ethReserve -= ethAmount;
+    }
 
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
-
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
-		}
-
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
-	}
-
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
-
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+    // Function to calculate output amount using x * y = k
+    function getAmountOut(uint inputAmount, uint inputReserve, uint outputReserve, uint total) public pure returns (uint) {
+        require(inputAmount > 0, "Invalid input amount");
+        require(inputReserve > 0 && outputReserve > 0, "Invalid reserves");
+        uint NewInputToken = inputAmount + inputReserve;
+        uint NewOutputToken = total / NewInputToken;
+        return outputReserve - NewOutputToken;
+    }
 }
